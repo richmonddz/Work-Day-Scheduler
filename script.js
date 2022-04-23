a1ccaeb915521c896e1efd7d858e37ae8911d0e6
Richmond Dominguez <richmonddominguez@gmail.com>
2022-04-23 02:17:29 -0700

    added color indicator for time, still not working, localstorage event added for save

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
+var saveBtn = $(".saveBtn");
 var currentDate = moment().format("LLLL");
 $("#currentDay").html(currentDate);
 
-$("saveBtn").on("click", function () {
-  var text = $(this).siblings("textHere").val();
-  var time = $(this).parent().attr("id");
+saveBtn.on("click", function () {
+  var time = $(this).siblings(".Time").text();
+  var todo = $(this).siblings(".ToDo").val();
 
-  localStorage.setItem(text, time);
-  $("#com-3pm .description").val(localStorage.getitem("com-3pm"));
+  localStorage.setItem(time, todo);
 });
+function ColorCode() {
+  var hour = moment().hours();
+
+  $(".time-block").each(function () {
+    var SetHour = parseInt($(this).attr("id"));
+
+    if (SetHour > hour) {
+      $(this).addClass("future");
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+    } else if (SetHour === hour) {
+      $(this).addClass("present");
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+    } else {
+      $(this).addClass("past");
+    }
+  });
+}
