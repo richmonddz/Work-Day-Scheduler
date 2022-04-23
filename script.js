0ab600426604659a5db2d0f0b36f5f5a889815ac
Richmond Dominguez <richmonddominguez@gmail.com>
2022-04-23 01:05:02 -0700

    new bootstrap, last boxes showed gaps

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 var currentDate = moment().format("LLLL");
 $("#currentDay").html(currentDate);
+
+$("saveBtn").on("click", function () {
+  var text = $(this).siblings("textHere").val();
+  var time = $(this).parent().attr("id");
+
+  localStorage.setItem(text, time);
+  $("#com-3pm .description").val(localStorage.getitem("com-3pm"));
+});
